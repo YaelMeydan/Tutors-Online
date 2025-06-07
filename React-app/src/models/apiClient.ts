49a54a5394ce28b377c39d9f359da011006e981f
YaelMeydan <yael.meydan@gmail.com>
2025-06-07 21:50:43 +0300

    Update app.ts 4

--- React-app/src/models/apiClient.ts
+++ React-app/src/models/apiClient.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const tokenKeyName = "token";
 
 export const apiClient = axios.create({
-    baseURL: 'https://tutors-online.onrender.com/',
+    baseURL: 'https://tutors-online.onrender.com',
 });
 
 apiClient.interceptors.request.use((config) => {
