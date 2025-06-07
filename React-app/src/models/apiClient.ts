be83d3cbe343429f3e9235b44c187c0a2f3d36b9
YaelMeydan <yael.meydan@gmail.com>
2025-06-07 20:30:05 +0300

    backend URL 10

--- React-app/src/models/apiClient.ts
+++ React-app/src/models/apiClient.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const tokenKeyName = "token";
 
 export const apiClient = axios.create({
-    baseURL: 'https://tutors-online.onrender.com/studentsPosts',
+    baseURL: 'https://tutors-online.onrender.com/index.ts',
 });
 
 apiClient.interceptors.request.use((config) => {
