b90a0299300bd3ca91e6b25baad17247d23efdb9
YaelMeydan <yael.meydan@gmail.com>
2025-06-07 20:00:24 +0300

    Update app.ts

--- React-Service/src/app.ts
+++ React-Service/src/app.ts
@@ -7,14 +7,14 @@ import { router as studentsPostsRouter } from "./routers/studentsPosts";
 
 export const app = express();
 
+app.use(json());
+
 app.use(cors({ origin: 'https://yaelmeydan.github.io' }));
 
 app.use((req, _, next) => {
     console.log(new Date(), req.method, req.url);
     next();
 });
- 
-app.use(json());
 
 app.use(express.static("public"));
 
