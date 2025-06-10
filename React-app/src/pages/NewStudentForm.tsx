83fec230c7990c2c56ccd8c7b68a829b15fef40f
YaelMeydan <yael.meydan@gmail.com>
2025-06-11 02:02:15 +0300

    Update NewStudentForm.tsx

--- React-app/src/pages/NewStudentForm.tsx
+++ React-app/src/pages/NewStudentForm.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-//import {NewStudentPost, type StudentRequest} from "../models/studentRequest";
+//import { useState } from "react";
+import { type StudentRequest} from "../models/studentRequest";
 import { Main } from "../components/Main";
 import { Input } from "../components/Input";
 import { PrimaryButton } from "../components/PrimaryButton";
@@ -8,55 +8,45 @@ import { useNavigate } from "react-router";
 
 export function NewStudentForm() {
     const navigate = useNavigate();
+    const studentRequest ={} as StudentRequest;
 
-    type StudentFormState = {
-        name: string;
-        subject: string;
-        level: string;
-        contact: string;
-    };
+    async function Submit(formData: FormData) {
+        const formEntries = Object.fromEntries(formData);
+        studentRequest.name = formEntries.name as string;
+        studentRequest.subject = formEntries.subject as string;
+        studentRequest.level = formEntries.level as string;
+        studentRequest.contact = formEntries.contact as string;
 
-    const [studentRequest, setStudentRequest] = useState<StudentFormState>({
-        name: "",
-        subject: "",
-        level: "",
-        contact: ""
-    });
+
+    }
 
     return(
     <Main>
          <h1>New Student Request</h1>
-            <form >
+            <form action={Submit}>
                 <Input
                     id="name"
                     label="Name"
                     name="name"
                     required
-                    value={studentRequest.name}
-                    onChange={e => setStudentRequest({ ...studentRequest, name: e.target.value })}
                 />
                 <Input
                     id="subject"
                     label="Subject"
                     name="subject"
                     required
-                    value={studentRequest.subject}
-                    onChange={e => setStudentRequest({ ...studentRequest, subject: e.target.value })}
+
                 />
                 <Input
                     id="level"
                     label="Level (optional)"
                     name="level"
-                    value={studentRequest.level}
-                    onChange={e => setStudentRequest({ ...studentRequest, level: e.target.value })}
                 />
                 <Input
                     id="contact"
                     label="Contact Information"
                     name="contact"
                     required
-                    value={studentRequest.contact}
-                    onChange={e => setStudentRequest({ ...studentRequest, contact: e.target.value })}
                 />
                 <PrimaryButton onClick={async () => {
                 try {
